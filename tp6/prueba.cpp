03a8eca96c597a41d26d5b143f59467e38862b98
Martinmisiones777 <martinmisiones@gmail.com>
2022-10-14 19:36:34 -0300

    matriz

---
+++ tp6/prueba.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int myMatriz1[5][5];
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 5; j++)
+        {
+            myMatriz1[i][j]=0;
+            cout<< myMatriz1[i][j] <<"\t" ;
+        }
+    cout<<"\n"; 
+    }
+    
+
+
+}
+
+ 
+	
