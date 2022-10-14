3b09f07cb5a305a93815269af48fd6d384e78606
Martinmisiones777 <martinmisiones@gmail.com>
2022-10-14 17:07:45 -0300

    vectores

---
+++ tp5/vector.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int vector[10];
+    int vector2[10];
+    int res=0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        vector[i]=10;
+        vector2[i]=5;
+        res=vector[i]+vector2[i];
+        cout<<res<<endl;
+    }
+    
+
+
+return 0;    
+}
